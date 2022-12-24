e2251978c20ff424153f9c599b68b5e0b77f81b2
Denny Kurniawan <dennykurniawanjr@gmail.com>
2022-12-25 01:00:27 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  StatusBar,
+  SafeAreaView,
+} from 'react-native';
+import { colors } from './src/utils/colors';
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+  text: {
+    color: colors.white,
   },
 });
---
+++ src/features/Focus.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+type Props = {};
+
+const Focus: React.FC<Props> = (props) => {
+  return (
+    <View>
+      <Text>Focus Features</Text>
+    </View>
+  );
+};
+
+export default Focus;
+
+const styles = StyleSheet.create({
+  container: {},
+});
