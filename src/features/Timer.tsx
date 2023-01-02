8b1e8e5e1e9ad3fd1299618061bbb11ca8f4657e
Denny Kurniawan <dennykurniawanjr@gmail.com>
2023-01-02 22:06:44 +0700

    part 43

--- src/features/Timer.tsx
+++ src/features/Timer.tsx
@@ -1,8 +1,9 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { useState } from 'react';
+import { ProgressBar } from 'react-native-paper';
 import Countdown from '../components/Countdown';
-import { sizes } from '../utils/sizes';
 import RoundedButton from '../components/RoundedButton';
+import { sizes } from '../utils/sizes';
 import { colors } from '../utils/colors';
 
 type Props = {
@@ -13,19 +14,27 @@ type Props = {
 
 const Timer: React.FC<Props> = ({ focusSubject, onTimerEnd, clearSubject }) => {
   const [isStarted, setIsStarted] = useState<Boolean>(false);
+  const [progress, setProgress] = useState<number>(1);
 
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
         <Countdown
           isPaused={!isStarted}
-          onProgress={() => {}}
+          onProgress={setProgress}
           onEnd={() => {}}
         />
         <View style={{ paddingTop: sizes.xxl }}>
           <Text style={styles.title}>Focusing on:</Text>
           <Text style={styles.task}>{focusSubject}</Text>
         </View>
+      </View>
+      <View style={{ paddingTop: sizes.sm }}>
+        <ProgressBar
+          progress={progress}
+          color={colors.progressBar}
+          style={{ height: sizes.sm }}
+        />
       </View>
       <View style={styles.buttonWrapper}>
         {isStarted ? (
